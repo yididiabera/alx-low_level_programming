826728b36d159cddc4a230f73685c16edccaf2b6
yididiabera <yididiabera87@gmail.com>
2023-12-29 09:59:24 -0800

    sizeof fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,9 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %d byte(s)\n" , sizeof(a));
-	printf("Size of a int: %d byte(s)\n" , sizeof(b));
-	printf("Size of a long int: %d byte(s)\n" , sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n" , sizeof(d));
-	printf("Size of a float: %d byte(s)\n" ,sizeof(e));
+	printf("Size of a char: %d byte(s)\n" , sizeof(char));
+	printf("Size of a int: %d byte(s)\n" , sizeof(int));
+	printf("Size of a long int: %d byte(s)\n" , sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n" , sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n" ,sizeof(float));
 	return(0);

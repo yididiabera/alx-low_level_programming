9eba7e7a7f8ec4592a5ec61343d8709a2a4a480d
yididiabera <yididiabera87@gmail.com>
2023-12-31 09:37:59 -0800

    size of datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,5 @@ printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
-	
 return (0);
 }

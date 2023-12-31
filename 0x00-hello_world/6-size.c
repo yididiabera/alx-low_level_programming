b4e4dc2564553da21c6ce36f1a0e24c2c73bb7ef
yididiabera <yididiabera87@gmail.com>
2023-12-31 01:55:08 -0800

    size of fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-    printf("Size of a char: %lu byte(s)\n" ,(unsigned long)sizeof(char));
-    printf("Size of a int: %lu byte(s)\n" ,(unsigned long)sizeof(int));
-    printf("Size of a long int: %lu byte(s)\n" ,(unsigned long)sizeof(long int));
-    printf("Size of a long long int: %lu byte(s)\n" , (unsigned long)sizeof(long long int));
-    printf("Size of a float: %lu byte(s)\n" ,(unsigned long)sizeof(float));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	
-    return (0);
+return (0);
 }

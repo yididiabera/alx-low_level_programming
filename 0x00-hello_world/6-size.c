7f9a8076c9eb2f03bba46758a80def6a582a6a1c
yididiabera <yididiabera87@gmail.com>
2023-12-31 01:41:24 -0800

    size of fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry point of the program
+ *
  * Return: Always success
  */
 
@@ -13,3 +14,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n" , (unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n" ,(unsigned long)sizeof(float));
 	return (0);
+}

12f5959efbfb0555df1a07bc66fa8d9be6c385b4
yididiabera <yididiabera87@gmail.com>
2024-01-01 14:53:48 -0800

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*int print_sign - print the sign of a number
+*@n: is the number to be checked
+*Return: Always 0(success)
+*/
+
+int int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}

c27ff3eb42c83de01443fdb8543e42937bda00f8
yididiabera <yididiabera87@gmail.com>
2024-01-01 09:55:23 -0800

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+int a, b, c;
+{
+for (a = 0; a < 10; a++)
+{
+for (int b = 0; b < 10; b++)
+{
+for (int c = 0; c < 10; c++)
+{
+if (c != b && b != a && a < b && b < c)
+{
+	putchar('0' + a);
+	putchar('0' + b);
+	putchar('0' + c);
+
+if (c + b + a != 9 + 8 + 7)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

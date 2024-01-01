609fce529ef637f56a8be7ef3b2d417faeb743a0
yididiabera <yididiabera87@gmail.com>
2024-01-01 15:03:04 -0800

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs(int) - absolute value of an integer
+*@n: is the number to be checked
+*Return: void
+*/
+
+int _abs(int)
+{
+int n;
+if (n < 0)
+	return (n * -1);
+return (n);
+}

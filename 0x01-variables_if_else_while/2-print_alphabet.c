ec6978855ffbbf748ed06bc2d628309311406d60
yididiabera <yididiabera87@gmail.com>
2024-01-01 08:02:43 -0800

    lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,12 @@
 *Return: Always 0
 */
 
-int main(void){
-    char ch;
-    for(ch = 'a'; ch <= 'z'; ch++)
-        putchar(ch);
-    putchar('\n');
-    return (0);
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+putchar('\n');
+return (0);
 }

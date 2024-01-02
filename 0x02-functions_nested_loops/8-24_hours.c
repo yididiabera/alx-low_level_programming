861c7dba8fd91c5d334a42c512c0cc9dcdad708b
yididiabera <yididiabera87@gmail.com>
2024-01-01 16:11:34 -0800

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*jack_bauer - print every minute
+*@n: is the number to be checked
+*Return: Always 0
+*/
+
+void jack_bauer(void)
+{
+int hr;
+int min;
+
+for (hr = 0; hr < 24; hr++)
+{
+for (min = 0; min < 60; min++)
+{
+	_putchar((hr / 10) + '0');
+	_putchar((hr % 10) + '0');
+	_putchar(':');
+	_putchar((min / 10) + '0');
+	_putchar((min % 10) + '0');
+	_putchar('\n');
+}
+}
+}

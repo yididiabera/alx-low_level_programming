91c924e8781c56f55fd89b91c79a01837b1fb8e8
yididiabera <yididiabera87@gmail.com>
2024-01-04 11:11:09 -0800

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_strncpy - copies a string
  *
@@ -10,18 +9,17 @@
  *Return:dest
  *
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
 for (i = 0; src[i] != '\0' && i < n; i++)
 {
-dest[i] = src[i];
+	dest[i] = src[i];
 }
 for (; i < n; i++)
 {
-dest[i] = '\0';
+	dest[i] = '\0';
 }
 return (dest);
 }

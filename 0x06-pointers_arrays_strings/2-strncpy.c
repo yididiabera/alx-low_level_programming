a6ac0d034ba978d3fa2abf2f64bf6ac297244615
yididiabera <yididiabera87@gmail.com>
2024-01-04 09:51:46 -0800

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,11 +17,11 @@ int i;
 
 for (i = 0; src[i] != '\0' && i < n; i++)
 {
-    dest[i] = src[i];
+dest[i] = src[i];
 }
 for (; i < n; i++)
 {
-    dest[i] = '\0';
+dest[i] = '\0';
 }
 return (dest);
 }

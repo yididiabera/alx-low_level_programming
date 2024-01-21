4226a2d3ddd669f800c4264a27f92b530f8f8478
Yididia Abera <yididiabera87@gmail.com>
2024-01-21 03:58:14 +0300

    Done creating: 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-int i;
-for (i = 48; i <= 57; i++)
-{
-	putchar(i);
-if (i != 57)
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }

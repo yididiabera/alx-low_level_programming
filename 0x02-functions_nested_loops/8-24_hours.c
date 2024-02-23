c7def70e5a2aa79a092f30650e0325c8972753ea
Yididia Abera <yididiabera87@gmail.com>
2024-02-23 23:26:58 +0300

    Done creating: 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,17 @@
 
 void jack_bauer(void)
 {
-int hr,  min;
+	int hr, min;
 
-for (hr = 0; hr < 24; hr++)
-{
-for (min = 0; min < 60; min++)
-{
-	_putchar((hr / 10) + '0');
-	_putchar((hr % 10) + '0');
-	_putchar(':');
-	_putchar((min / 10) + '0');
-	_putchar((min % 10) + '0');
-	_putchar('\n');
-}
-}
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+		}
+	}
 }

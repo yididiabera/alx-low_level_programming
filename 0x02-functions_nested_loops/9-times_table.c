ddcc75ef8e2d1b7d46cb5035f740ac7e21f74b3b
Yididia Abera <yididiabera87@gmail.com>
2024-02-24 01:01:09 +0300

    Done creating: 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,39 +1,41 @@
 #include "main.h"
+
 /**
- * times_table - Print 9 times table
- * Return: Void.
+ *
  */
 
 void times_table(void)
 {
-int x, y, value;
+	int i, j, prod;
 
-for (y = 0; y < 10; y++)
-{
-for (x = 0; x < 10; x++)
-{
-value = x * y;
-if (x == 0)
-{
-_putchar(value + '0');
-}
-else if (value >= 10)
-{
-_putchar(' ');
-_putchar(value / 10 + '0');
-_putchar(value % 10 + '0');
-}
-else
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(value + '0');
-}
-if (x != 9)
-{
-_putchar(',');
-}
-}
-_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			prod = i * j;
+
+			if (i == 0)
+			{
+				_putchar(prod + '0');
+			}
+			else if (prod >= 10)
+			{
+				_putchar('0');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+
+			if (prod != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }

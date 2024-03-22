2fb820bee817cd0872f60fea86323888bdb08c23
Jedidiah Abera <yididi@Jedidiah>
2024-03-22 23:55:04 +0300

    done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,24 +11,24 @@
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex;
+	char locker[7], *codex;
 
 	int len = strlen(argv[1]), i, tmp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+	locker[0] = codex[tmp];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
 		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
+	locker[1] = codex[(tmp ^ 79) & 63];
 
 	tmp = 1;
 	for (i = 0; i < len; i++)
 		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
+	locker[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
@@ -37,18 +37,18 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 			tmp = argv[1][i];
 	}
 	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
+	locker[3] = codex[rand() & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
 		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
+	locker[4] = codex[(tmp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
 		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
+	locker[5] = codex[(tmp ^ 229) & 63];
 
-	password[6] = '\0';
-	printf("%s", password);
+	locker[6] = '\0';
+	printf("%s", locker);
 	return (0);
 }

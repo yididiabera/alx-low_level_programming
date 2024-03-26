10f156958708fa30d24bfe403d0d9dbad989f971
Jedidiah Abera <yididi@Jedidiah>
2024-03-26 03:12:13 +0300

    Done creating: cfuns.h

---
+++ 0x18-dynamic_libraries/cfuns.h
@@ -0,0 +1,13 @@
+#ifndef CFUNS_H
+#define CFUNS_H
+
+#include <stdio.h>
+
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* CFUNS_H */ 

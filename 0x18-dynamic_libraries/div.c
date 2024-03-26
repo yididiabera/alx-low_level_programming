505108eb9132fa95c443a3ef3480dae5d856aff9
Yididia Abera <yididiabera87@gmail.com>
2024-03-26 03:12:14 +0300

    Done creating: div.c

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "cfuns.h"
+/**
+ * div - it divides the dividened by the divisor.
+ *@a: integer passed to the function.
+ *@b: integer passed to the function.
+ * Return: returns the quationt of the two integers.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}

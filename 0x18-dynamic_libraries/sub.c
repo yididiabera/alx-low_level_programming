fc145039307b7510881912ad9b9b2727da1d36bc
Jedidiah Abera <yididi@Jedidiah>
2024-03-26 03:12:23 +0300

    Done creating: sub.c

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "cfuns.h"
+/**
+ * sub - the function to substract one int from another.
+ *@a: integers passed to the function.
+ *@b: integers passed to the function.
+ * Return: returns difference.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}

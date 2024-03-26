b1d6b3bd1696ad1a916095dcdfd3f2894e4b8a9c
Yididia Abera <yididiabera87@gmail.com>
2024-03-26 03:12:21 +0300

    Done creating: mul.c

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "cfuns.h"
+/**
+ * mul - it multiplies two integers.
+ *@a: integer passed to the function.
+ *@b: integer passed to the function.
+ * Return: returns the product of two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

8f0ed9ee53bf7b67d2550cb563bdde5e707e24f3
Yididia Abera <yididiabera87@gmail.com>
2024-03-26 03:12:11 +0300

    Done creating: add.c

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "cfuns.h"
+/**
+ * add - the function to add two integers
+ *@a: integer passed to the function.
+ *@b: integer passed to the function.
+ * Return: returns the sum of two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

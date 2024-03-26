061052b1e46fc299ae194ffc5cff9c89385d0d62
Jedidiah Abera <yididi@Jedidiah>
2024-03-26 03:12:20 +0300

    Done creating: mod.c

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "cfuns.h"
+/**
+ * mod - calculates the remainder.
+ * @a: integer passed to the function.
+ * @b: integer passed to the function.
+ * Return: returns remainder.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

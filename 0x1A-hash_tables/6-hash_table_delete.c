73491273b746fe41f07c108c2a10043fc7ed57c6
Yididia Abera <yididiabera87@gmail.com>
2024-04-13 01:13:00 +0300

    Done creating: 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
+
+		while (current != NULL)
+		{
+			hash_node_t *temp = current->next;
+
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}

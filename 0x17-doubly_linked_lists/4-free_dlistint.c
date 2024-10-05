94610cf1f63ee72e14db75a32a7704f69f1f7eff
Yididia Abera <yididiabera87@gmail.com>
2024-10-05 20:04:21 +0300

    Fixed minor issues

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,13 +10,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp;
+    dlistint_t *tmp;
 
-	while (head)
-	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
-	}
-
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }

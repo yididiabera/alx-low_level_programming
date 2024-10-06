2425535b9543c3cb837245a5646027373cc106c1
Yididia Abera <yididiabera87@gmail.com>
2024-10-06 22:36:30 +0300

    fixed minor issues

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,12 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+    list_t *tmp;
 
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+    while (head)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
 }
